5a90cf52881037e76c86ce23179d56777fb94987
Julio Nuila <julionuila2003@gmail.com>
2020-10-03 01:24:58 -0600

    First commit

---
+++ Fleet-Prime/src/main/java/com/FleetPrime/Fleet/Prime/FleetPrimeApplication.java
@@ -0,0 +1,13 @@
+package com.FleetPrime.Fleet.Prime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetPrimeApplication.class, args);
+	}
+
+}
---
+++ Fleet-Prime/src/test/java/com/FleetPrime/Fleet/Prime/FleetPrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FleetPrime.Fleet.Prime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetPrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
